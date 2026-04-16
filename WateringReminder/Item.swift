7b496e9ab974154809ef8ac1224e3d6ab90d21a6
kelleno96 <kellen.oconnor01@gmail.com>
2026-04-15 20:42:52 -0700

    Initial Commit

---
+++ WateringReminder/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WateringReminder
+//
+//  Created by Kellen O'Connor on 4/15/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WateringReminderTests/WateringReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  WateringReminderTests.swift
+//  WateringReminderTests
+//
+//  Created by Kellen O'Connor on 4/15/26.
+//
+
+import Testing
+@testable import WateringReminder
+
+struct WateringReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
